d786b1e19e3062426048e219acd63ab15f44ef28
cyberpunk_chris <143149878+Cyberpunkgfmimign@users.noreply.github.com>
2024-07-21 15:42:09 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,22 +1,22 @@
 //base by Tech-God
 //re-upload? recode? copy code? give credit ya :)
 //YouTube: @techgod143
-//Instagram: techgod143
+//Instagram: cyberpunk_chris
 //Telegram: t.me/techgod143
 //GitHub: @techgod143
-//WhatsApp: +917466008456
+//WhatsApp: ++254111446584
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "+254111446584"
+global.ownername = "༒𝑪𝒀𝑩𝑬𝑹𝑩𝑶𝑻༒"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
+global.ownernumber = '254111446584'  //creator number
 global.ownername = '🐛Tech god' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
